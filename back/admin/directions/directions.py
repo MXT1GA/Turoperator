8139d5e3631d16bccb71aadbc0c164ac7cf7d60f
MXT1GA <155301633+MXT1GA@users.noreply.github.com>
2024-12-16 22:24:39 +0700

    fix folders

--- back/admin/directions/directions.py
+++ back/admin/directions/directions.py
@@ -1,7 +1,7 @@
 from back.db import db_api as db
 
 
-def createDirections():
+def createDirections(title, date, cost):
     return db.createDirections(title, date, cost)
 def listDirections():
     return db.listDirections()
